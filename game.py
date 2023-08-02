ef2a5917ccbc88d846aaa47e23428df1303e3f8f
Kishan Karavadi <kishan.karavadi@gmail.com>
2023-08-02 17:15:09 -0400

    screen scaling

--- game.py
+++ game.py
@@ -11,15 +11,11 @@ class Game:
         pygame.display.set_caption("Naruto")
         self.screen = pygame.display.set_mode((640, 480))
 
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
 
-        # self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        # self.img.set_colorkey((0, 0, 0))
-
-        # self.img_pos = [160, 260]
         self.movement = [False, False]
-
-        # self.collision_area = pygame.Rect(50, 50, 300, 50)
 
         self.assets = {
             'player': load_image('entities/player.png')
@@ -29,24 +25,9 @@ class Game:
 
     def run(self) -> None:
         while True:
-            self.screen.fill((14, 219, 248))
-
-            # img_r = pygame.Rect(
-            #     self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-
-            # # Or you could SPLAT pygame.Rect(*self.img_pos, *self.img.get_size())
-
-            # if img_r.colliderect(self.collision_area):
-            #     pygame.draw.rect(self.screen, (0, 100, 255),
-            #                      self.collision_area)
-            # else:
-            #     pygame.draw.rect(self.screen, (0, 50, 155),
-            #                      self.collision_area)
-
-            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            # self.screen.blit(self.img, self.img_pos)
+            self.display.fill((14, 219, 248))
             self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -64,7 +45,8 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-
+            self.screen.blit(pygame.transform.scale(
+                self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
