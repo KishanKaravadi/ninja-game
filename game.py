026bb35ac56234c638a7174b1306f861456552df
Kishan Karavadi <kishan.karavadi@gmail.com>
2023-08-03 17:26:52 -0400

    physics and basic sandbox game

--- game.py
+++ game.py
@@ -36,7 +36,8 @@ class Game:
 
             self.tilemap.render(self.display)
 
-            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.update(
+                self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
 
             for event in pygame.event.get():
@@ -49,12 +50,15 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+
             self.screen.blit(pygame.transform.scale(
                 self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
